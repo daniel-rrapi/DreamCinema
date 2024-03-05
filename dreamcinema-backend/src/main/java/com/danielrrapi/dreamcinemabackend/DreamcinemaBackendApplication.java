1ca9c2fb336fd8924e37c046021c9ad1686bc6b6
Daniel Rrapi <danielrrapi@proton.me>
2024-03-05 20:03:37 +0100

    Created frontend and backend folders

---
+++ dreamcinema-backend/src/main/java/com/danielrrapi/dreamcinemabackend/DreamcinemaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.danielrrapi.dreamcinemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamcinemaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamcinemaBackendApplication.class, args);
+	}
+
+}
---
+++ dreamcinema-backend/src/test/java/com/danielrrapi/dreamcinemabackend/DreamcinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielrrapi.dreamcinemabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamcinemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
